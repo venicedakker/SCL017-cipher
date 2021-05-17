7efde730dae819ad8a9c802be42a8be0ca45fa97
Valeria Argomedo <valeria.argomedo@gmail.com>
2021-05-17 13:31:41 -0400

    Segundo commit, día 8, Agrego el esqueleto del HTML, css e inicio de js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+// click del boton de cifrado - llega a la pagina para poner el texto y offset
+document.getElementById("cpr-btn").addEventListener('click', () => {
+    document.getElementById("explanation-card").style.display = "none";
+    document.getElementById("question-card").style.display = "block";
+    document.getElementById("answer-card").style.display = "block";
+});
+
+//click en boton de descifrado - llega a la pagina para poner el texto y offset
+document.getElementById("dcpr-btn").addEventListener('click', () => {
+    document.getElementById("explanation-card").style.display = "none";
+    document.getElementById("question-card").style.display = "block";
+    document.getElementById("answer-card").style.display = "block";
+});
+
+// Obtengo el texto dentro del input
+var texto = document.getElementById("txt");
+var sbt_btn = document.getElementById("submit-btn");
+
+sbt_btn.addEventListener("click",()=>{
+console.log(texto.value)
+}); 
