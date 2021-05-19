98094765559bde5101f7ffaabba26932877f13ff
Valeria Argomedo <valeria.argomedo@gmail.com>
2021-05-19 18:57:07 -0400

    Commit 1er intento de Cipher js

--- cipher.js
+++ cipher.js
@@ -1,5 +1,29 @@
 const cipher = {
-  // ...
+    crypto:function crypto(txt, offset){
+
+    const output = "";
+
+    let c;
+
+    for (i = 0; i < txt.length(); i++) {
+      let aux1 = txt.charCodeAt(i);
+      
+      if (aux1 >= 65 && aux1 <= 90) {
+        c = String.fromCharCode(((aux1 - 65 + offset) % 26) + 65);
+      } else if (aux1 >= 97 && aux1 <= 122) {
+        c = String.fromCharCode(((aux1 - 97 + offset) % 26) + 97);
+      }
+      output += c;
+    }
+    return output;
+    
+  }
+
+
+
+
+
+
 };
 
 export default cipher;
--- index.js
+++ index.js
@@ -2,11 +2,18 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
+
+// Diferencia en el texto de las Casilla para ingresar el texto
+var txt1 = ["Cifremos !!", "Descifremos !!"];
+var aux = document.getElementById("title-question");
+
+
 // click del boton de cifrado - llega a la pagina para poner el texto y offset
 document.getElementById("cpr-btn").addEventListener('click', () => {
     document.getElementById("explanation-card").style.display = "none";
     document.getElementById("question-card").style.display = "block";
     document.getElementById("answer-card").style.display = "block";
+    aux.innerText = txt1[0];
 });
 
 //click en boton de descifrado - llega a la pagina para poner el texto y offset
@@ -14,12 +21,19 @@ document.getElementById("dcpr-btn").addEventListener('click', () => {
     document.getElementById("explanation-card").style.display = "none";
     document.getElementById("question-card").style.display = "block";
     document.getElementById("answer-card").style.display = "block";
+    aux.innerText = txt1[1];
 });
 
 // Obtengo el texto dentro del input
 var texto = document.getElementById("txt");
 var sbt_btn = document.getElementById("submit-btn");
+var texto2 = document.getElementById("answer-text");
+var offset = document.getElementById("offset");
 
-sbt_btn.addEventListener("click",()=>{
-console.log(texto.value)
-}); 
+sbt_btn.addEventListener("click", () => {
+  console.log("texto: " + texto.value);
+  console.log("offset: " + offset.value);
+  texto2.innerText= texto.value;
+  var aa = cipher.crypto(texto.value, offset.value);
+  console.log(aa);
+});
