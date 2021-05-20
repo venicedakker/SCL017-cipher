2b42192b48cc5e7a12bbc30678e2ab4b61b85404
Valeria Argomedo <valeria.argomedo@gmail.com>
2021-05-19 20:21:05 -0400

    funciona crypto

--- cipher.js
+++ cipher.js
@@ -1,17 +1,18 @@
 const cipher = {
   crypto: function crypto(txt, offset) {
     let output ="";
+    offset = parseInt(offset);
     let c = "";
-
+    let aux1;
     for (let i = 0; i < txt.length; i++) {
-      let aux1 = txt.charCodeAt(i);
+      aux1 = txt.charCodeAt(i);
 
       if (aux1 >= 65 && aux1 <= 90) {
         c = String.fromCharCode(((aux1 - 65 + offset) % 26) + 65);
       } else if (aux1 >= 97 && aux1 <= 122) {
         c = String.fromCharCode(((aux1 - 97 + offset) % 26) + 97);
       }
-      
+
       output+=c;
     }
     return output;
