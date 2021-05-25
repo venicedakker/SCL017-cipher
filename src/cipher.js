e7e561a1ba3bf2c22d05f9c53d1b42dafde76f28
Valeria Argomedo <valeria.argomedo@gmail.com>
2021-05-25 08:40:45 -0400

    still not fixed the error in decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,6 @@
 const cipher = {
   encode: function encode(offset, txt) {
+    offset = parseInt(offset);
     if (typeof offset!== "number"){
       throw new TypeError ("Error: offset not a number");
     }
@@ -20,6 +21,8 @@ const cipher = {
         cChar = String.fromCharCode(((char - 97 + offset) % 26) + 97);
       } else if (char == 32) {
         cChar = " ";
+      }else{
+        cChar = String.fromCharCode(char);
       }
 
       output += cChar;
@@ -28,6 +31,8 @@ const cipher = {
     return output;
   },
   decode: function decode(offset, txt) {
+    offset = parseInt(offset);
+
     if (typeof offset!== "number"){
       throw new TypeError ("Error: offset not a number");
     }
@@ -35,21 +40,23 @@ const cipher = {
       throw new TypeError("Error: text not a string");
     }
 
-    offset = (26 - offset) % 26;
+    const shift = (26 - offset) % 26;
     
     let output = "";
     let cChar = "";
     let char = "";
-        
+
     for (let i = 0; i < txt.length; i++) {
       char = txt.charCodeAt(i);
 
       if (char >= 65 && char <= 90) {
-        cChar = String.fromCharCode(((char - 65 + offset) % 26) + 65);
+        cChar = String.fromCharCode(((char - 65 + shift) % 26) + 65);
       } else if (char >= 97 && char <= 122) {
-        cChar = String.fromCharCode(((char - 97 + offset) % 26) + 97);
+        cChar = String.fromCharCode(((char - 97 + shift) % 26) + 97);
       } else if (char == 32) {
         cChar = " ";
+      }else{
+        cChar = String.fromCharCode(char);
       }
 
       output += cChar;
